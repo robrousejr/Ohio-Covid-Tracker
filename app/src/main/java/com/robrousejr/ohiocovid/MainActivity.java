e061c0b1fcdd01aca32fc6bd75811500f97f14f9
Rob Rouse <robrousejr@gmail.com>
2020-07-18 16:58:34 -0400

    Add ability to scrape
      - Add Android permission to use internet
      - Create thread which can access website

--- app/src/main/java/com/robrousejr/ohiocovid/MainActivity.java
+++ app/src/main/java/com/robrousejr/ohiocovid/MainActivity.java
@@ -3,13 +3,57 @@ package com.robrousejr.ohiocovid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite();
+            }
+        });
     }
+
+    public void getWebsite() {
+        Log.i("Info", "getWebsite()");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                   Document doc = Jsoup.connect("https://covidtracking.com/").get();
+                   String title = doc.title();
+                   builder.append(title);
+                } catch (IOException e) {
+                    Log.e("Error", e.getMessage());
+                }
+
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("Info", builder.toString());
+                    }
+                });
+            }
+
+        }).start();
+    }
+
 }
